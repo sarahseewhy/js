3ae432644add1fa656a9caf181451e72cb59cb64
Sarah C Young <sarahseewhy@gmail.com>
2014-07-24 16:28:39 +0100

    Adds refreshing stats to render function

--- views/app.js
+++ views/app.js
@@ -31,9 +31,35 @@ app.AppView = Backbone.View.extend({
     this.listenTo(app.Todos, 'change:completed', this.filterOne); 
     this.listenTo(app.Todos,'filter', this.filterAll); 
     this.listenTo(app.Todos, 'all', this.render);
-    
+
     app.Todos.fetch();
   },
+
+  render: function() {
+    var completed = app.Todos.completed().length; 
+    var remaining = app.Todos.remaining().length;
+
+    if ( app.Todos.length ) {
+      this.$main.show();
+      this.$footer.show();
+
+      this.$footer.html(this.statsTemplate({
+        completed: completed,
+        remaining: remaining
+      }));
+
+      this.$('#filters li a')
+        .removeClass('selected')
+        .filter('[href="#/' + ( app.TodoFilter || '' ) + '"]')
+        .addClass('selected');
+    } else {
+      this.$main.hide();
+      this.$footer.hide();
+    }
+
+    this.allCheckbox.checked = !remaining;
+  },
+
 
   // Add a single todo item to the list by creating a view for it, and 
   // appending its element to the `<ul>`.
@@ -50,5 +76,8 @@ app.AppView = Backbone.View.extend({
     app.Todos.each(this.addOne, this);
   }
 
+  filterOne : function (todo) { 
+    todo.trigger('visible');
+  },
 
 });
