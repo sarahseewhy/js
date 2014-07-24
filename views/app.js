77f4be84f2a1e6ae2d4c5604ca4152c651da60d7
Sarah C Young <sarahseewhy@gmail.com>
2014-07-24 16:17:20 +0100

    Adds new bindings to relevant events

--- views/app.js
+++ views/app.js
@@ -27,6 +27,12 @@ app.AppView = Backbone.View.extend({
 
   	this.listenTo(app.Todos, 'add', this.addOne);
   	this.listenTo(app.Todos, 'reset', this.addAll);
+
+    this.listenTo(app.Todos, 'change:completed', this.filterOne); 
+    this.listenTo(app.Todos,'filter', this.filterAll); 
+    this.listenTo(app.Todos, 'all', this.render);
+    
+    app.Todos.fetch();
   },
 
   // Add a single todo item to the list by creating a view for it, and 
